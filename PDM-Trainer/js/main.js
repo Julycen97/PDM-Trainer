96a1a9e5ec86c151f8c2ddf4fafc910ef2405fef
Julian Centurion <july_cen@hotmail.com.ar>
2024-10-30 07:20:59 -0300

    Rename style classes with Spanish names to English names

--- PDM-Trainer/js/main.js
+++ PDM-Trainer/js/main.js
@@ -1,51 +1,50 @@
 const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
 
 const itemA = document.querySelector("#planes");;
-const itemB = document.querySelector("#recetas");;
+const itemB = document.querySelector("#recipes");;
 const itemC = document.querySelector("#tips");;
-const itemD = document.querySelector("#contacto");;
+const itemD = document.querySelector("#contact");;
 
-const motivation = document.querySelector(".Motivation");
-const main = document.querySelector(".Main");
-const aside = document.querySelector(".Aside");
-const footer = document.querySelector(".Footer");
+const motivation = document.querySelector(".motivation");
+const main = document.querySelector(".main");
+const aside = document.querySelector(".aside");
+const footer = document.querySelector(".footer");
 
 
-
-abrir.addEventListener("click", () => {
-    nav.classList.add("Visible");
+ open.addEventListener("click", () => {
+    nav.classList.add("visible");
     
-    motivation.classList.add("HiddenAll");
-    main.classList.add("HiddenAll");
-    aside.classList.add("HiddenAll");
-    footer.classList.add("HiddenAll");
+    motivation.classList.add("hiddenAll");
+    main.classList.add("hiddenAll");
+    aside.classList.add("hiddenAll");
+    footer.classList.add("hiddenAll");
 })
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("Visible");
+close.addEventListener("click", () => {
+    nav.classList.remove("visible");
 
-    motivation.classList.remove("HiddenAll");
-    main.classList.remove("HiddenAll");
-    aside.classList.remove("HiddenAll");
-    footer.classList.remove("HiddenAll");
+    motivation.classList.remove("hiddenAll");
+    main.classList.remove("hiddenAll");
+    aside.classList.remove("hiddenAll");
+    footer.classList.remove("hiddenAll");
 })
 
 itemA.addEventListener("click", () => {
-  nav.classList.remove("Visible");
+  nav.classList.remove("visible");
 })
 
 itemB.addEventListener("click", () => {
-  nav.classList.remove("Visible");
+  nav.classList.remove("visible");
 })
 
 itemC.addEventListener("click", () => {
-  nav.classList.remove("Visible");
+  nav.classList.remove("visible");
 })
 
 itemD.addEventListener("click", () => {
-  nav.classList.remove("Visible");
+  nav.classList.remove("visible");
 })
 
 
